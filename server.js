da162ca664c490e14268127410692d554ee71257
ch6vip <ch6vip@gmail.com>
2026-03-23 19:07:35 +0800

    修复服务器监听地址，确保在 127.0.0.1 上运行

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`CF-TTS server running on http://localhost:${PORT}`);
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`CF-TTS server running on http://127.0.0.1:${PORT}`);
 });
